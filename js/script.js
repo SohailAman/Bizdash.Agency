f54b294afee015f1557619095ca18b8e472c5468
SohailAman <101714964+SohailAman@users.noreply.github.com>
2022-10-05 03:28:07 +0500

    Initial commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     grabCursor: true,
     slidesPerView: 4,
     spaceBetween: 30,
-    slidesPerGroup: 4,
+    slidesPerGroup: 1,
     freeMode: true,
     speed: 900,
     loopFillGroupWithBlank: true,
@@ -10,4 +10,22 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-});
+    breakpoints: {
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        998: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+        1210: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+}); 
